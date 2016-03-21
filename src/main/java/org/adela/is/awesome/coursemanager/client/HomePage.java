420bf7151487ccad4c7bb7ed0f86c77dd417328b
Kevin Gaasch <gaaschk@gmail.com>
2016-03-21 16:07:34 -0400

    upgraded swagger version

---
+++ src/main/java/org/adela/is/awesome/coursemanager/client/HomePage.java
@@ -0,0 +1,14 @@
+package org.adela.is.awesome.coursemanager.client;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.UI;
+
+public class HomePage extends UI {
+	private static final long serialVersionUID = -7349115858008610032L;
+
+	@Override
+	protected void init(VaadinRequest request) {
+		
+	}
+
+}
---
+++ src/main/java/org/adela/is/awesome/coursemanager/client/StudentView.java
@@ -0,0 +1,15 @@
+package org.adela.is.awesome.coursemanager.client;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.VerticalLayout;
+
+public class StudentView extends VerticalLayout implements View {
+
+	
+	
+	@Override
+	public void enter(ViewChangeEvent event) {
+	}
+
+}
