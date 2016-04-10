bb11ba7d593281312582a78f4de41fe96ea63933
Kevin Gaasch <gaaschk@gmail.com>
2016-04-10 11:06:02 -0400

    word

--- src/main/java/org/adela/is/awesome/coursemanager/client/StudentView.java
+++ src/main/java/org/adela/is/awesome/coursemanager/client/StudentView.java
@@ -3,6 +3,8 @@ package org.adela.is.awesome.coursemanager.client;
 import org.adela.is.awesome.coursemanager.persistence.entity.StudentEntity;
 import org.adela.is.awesome.coursemanager.persistence.repository.StudentEntityRepository;
 
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
@@ -21,6 +23,7 @@ public class StudentView extends HorizontalSplitPanel{
 
 	private Table studentDataTable;
 	private TextField nameField = new TextField();
+	private StudentEntity selectedStudent;
 	
 	public StudentView(StudentEntityRepository studentRepo){
 		this.studentRepo = studentRepo;
@@ -30,6 +33,14 @@ public class StudentView extends HorizontalSplitPanel{
 		studentDataTable.setSelectable(true);
 		studentDataTable.setImmediate(true);
 		studentDataTable.addContainerProperty("Name", String.class, null);
+		studentDataTable.addValueChangeListener(new ValueChangeListener() {
+			private static final long serialVersionUID = 9162327903714422573L;
+
+			@Override
+			public void valueChange(ValueChangeEvent event) {
+				updateSelectedStudent();
+			}
+		});
 		this.reloadTableData();
 		tablePanel.addComponent(studentDataTable);
 		HorizontalLayout tableButtonPanel = new HorizontalLayout();
@@ -54,9 +65,24 @@ public class StudentView extends HorizontalSplitPanel{
 			}}));
 		tablePanel.addComponent(tableButtonPanel);
 		this.addComponent(tablePanel);
-		GridLayout formPanel = new GridLayout(2,1);
-		formPanel.addComponent(new Label("Strudent Name:"),0,0);
-		formPanel.addComponent(nameField,1,0);
+		VerticalLayout formPanel = new VerticalLayout();
+		GridLayout formDataPanel = new GridLayout(2,1);
+		formDataPanel.addComponent(new Label("Strudent Name:"),0,0);
+		formDataPanel.addComponent(nameField,1,0);
+		formPanel.addComponent(formDataPanel);
+		Button saveButton = new Button("Save", new ClickListener() {
+			private static final long serialVersionUID = -3076728302768059993L;
+
+			@Override
+			public void buttonClick(ClickEvent event) {
+				if(selectedStudent!=null){
+					selectedStudent.setStudentName(nameField.getValue());
+					studentRepo.save(selectedStudent);
+					reloadTableData();
+				}
+			}
+		});
+		formPanel.addComponent(saveButton);
 		this.addComponent(formPanel);
 	}
 	
@@ -66,5 +92,14 @@ public class StudentView extends HorizontalSplitPanel{
 		allStudents.forEach(s -> studentDataTable.addItem(new Object[]{s.getStudentName()}, s.getStudentEntityId()));
 		studentDataTable.setPageLength(studentDataTable.size());
 		studentDataTable.setValue(studentDataTable.getCurrentPageFirstItemId());
+		this.markAsDirty();
+	}
+	
+	private void updateSelectedStudent(){
+		Long selectedStudentId =  (Long)studentDataTable.getValue();
+		if(selectedStudentId!=null){
+			selectedStudent = studentRepo.findOne(selectedStudentId);
+			nameField.setValue(selectedStudent.getStudentName());
+		}
 	}
 }
