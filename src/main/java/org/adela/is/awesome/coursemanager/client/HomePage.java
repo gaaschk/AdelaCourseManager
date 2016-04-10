e8164d40975260f96a720425f5244d108bcfdd2c
Kevin Gaasch <gaaschk@gmail.com>
2016-04-10 14:39:20 -0400

    Adela is an angry bunny

--- src/main/java/org/adela/is/awesome/coursemanager/client/HomePage.java
+++ src/main/java/org/adela/is/awesome/coursemanager/client/HomePage.java
@@ -5,12 +5,14 @@ import org.adela.is.awesome.coursemanager.persistence.repository.TeacherEntityRe
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vaadin.annotations.Theme;
+import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Image;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -35,9 +37,13 @@ public class HomePage extends UI {
 		//content.setSizeFull();
 		this.setContent(content);
 		
+		HorizontalLayout headerPanel = new HorizontalLayout();
+		Image bunnyImage = new Image(null, new ThemeResource("img/angry-bunny.jpg"));
+		headerPanel.addComponent(bunnyImage);
 		Label headerLabel = new Label("Adela is an Angry Bunny!");
 		headerLabel.setStyleName("headerLabel");
-		content.addComponent(headerLabel);
+		headerPanel.addComponent(headerLabel);
+		content.addComponent(headerPanel);
 		
 		HorizontalLayout menuBar = new HorizontalLayout();
 		content.addComponent(menuBar);
